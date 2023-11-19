989eafeba152e1aa131fcdbdd0893f1274095a67
Maciej Góralczyk <goralczm@gmail.com>
2023-11-19 01:41:37 +0100

    Add Factory Pattern

---
+++ Assets/_Scripts/Factory Pattern/Factory.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+public static class StatusEffectFactory<T> where T : FactoryItem
+{
+    private static Dictionary<string, Type> _itemsByName;
+    private static bool IsInitialized => _itemsByName != null;
+
+    private static void InitializeFactory()
+    {
+        if (IsInitialized)
+            return;
+
+        var itemType = Assembly.GetAssembly(typeof(T)).GetTypes()
+            .Where(myType => myType.IsClass && !myType.IsAbstract && myType.IsSubclassOf(typeof(T)));
+
+        _itemsByName = new Dictionary<string, Type>();
+
+        foreach (var type in itemType)
+        {
+            var tempItem = Activator.CreateInstance(type) as T;
+            _itemsByName.Add(tempItem.Name, type);
+        }
+    }
+
+    public static T GetItem(string name)
+    {
+        InitializeFactory();
+
+        if (!_itemsByName.ContainsKey(name))
+            return null;
+
+        Type type = _itemsByName[name];
+        var item = Activator.CreateInstance(type) as T;
+        return item;
+    }
+
+    internal static IEnumerable<string> GetItemNames()
+    {
+        InitializeFactory();
+        return _itemsByName.Keys;
+    }
+}
---
+++ Assets/_Scripts/Factory Pattern/FactoryItem.cs
@@ -0,0 +1,4 @@
+public abstract class FactoryItem
+{
+    public abstract string Name { get; }
+}
